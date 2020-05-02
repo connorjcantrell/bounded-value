f1762d70a3b46ce33bbc6356a2f8f08d4ee25582
connorjcantrell <connorcantrell@outlook.com>
2020-05-01 22:47:11 -0700

    Refactored functions and tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,116 +2,73 @@
 use rand::Rng;
 use std::fmt;
 
-// Contemplating my design choice:
-
-// 1) What are your thoughts on my implementations for the `Range` struct? Would it have been better
-// to write an entirely separate trait instead? 
-//
-// 2) What are your thoughts on `RangeError`? Would you have written a custom error differently? If 
-// `RangeError` is acceptable, could it be improved upon? 
-//
-// 3) I greatly appreciate advice on improving anything else that jumps out at you.
-
-
-// Reason for building this:
-
-// I assume something like this already exists, but I thought it would be a good exercise. This struct 
-// will be used inside a game I am building. I need a way to represent a range that knows its current
-// location inside a defined range. This range could represent a players skill for a particular attribute. 
-// 
-// For instance, I could define an attribute called `confidence` w/ a min = 0, max = 100, and current = 50.
-// As the player interacts with game, their confidence can increase or decrease depending on the results
-// of an event. 
-//
-// If you know of a simpler way to achieve this, please share :)
-
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
-pub struct Range {
-    pub min: u16,
-    pub max: u16,
-    pub current: u16
+pub struct BoundedValue {
+    min: u16,
+    max: u16,
+    value: u16
 }
 
-impl Range {
-    pub fn new(min: u16, max: u16, current: u16) -> Self {  // -> Result<Range, RangeError>
+impl BoundedValue {
+    pub fn new(min: u16, max: u16, value: u16) -> Self {  // -> Result<BoundedValue, RangeError>
         // Assert min < max, throw relevant error message
-        // Assert current >= min and <= max, throw relevant error message
-        Range{ min, max, current }
+        // Assert value >= min and <= max, throw relevant error message
+        BoundedValue{ min, max, value }
     }
     
-    pub fn increase(mut self) -> Result<u16, RangeError> {
-        let value = match self.current.overflowing_add(1) {
-            (_, true) => self.max,
-            (value, false) => value,
+    pub fn increase(&mut self) -> Result<u16, RangeError> {
+        let value = match self.value.checked_add(1) {
+            Some(value) => value,
+            None => self.max,
         };
-        if value > self.max {
-            self.current = self.max;
-        } else {
-            self.current = value;
-        }
-
-        Ok(self.current)
+        self.value = value.min(self.max);
+        Ok(self.value)
     }
 
-    pub fn decrease(mut self) -> Result<u16, RangeError> {
-        let value = match self.current.overflowing_sub(1) {
-            (_, true) => self.min,
-            (value, false) => value,
+    pub fn decrease(&mut self) -> Result<u16, RangeError> {
+        let value = match self.value.checked_sub(1) {
+            Some(value) => value,
+            None => self.min,
         };
-        if value < self.min {
-            self.current = self.min;
-        } else {
-            self.current = value;
-        }
-
-        Ok(self.current)
+        self.value = value.min(self.max);
+        Ok(self.value)
     }
     
-    pub fn increase_by(mut self, amount: u16) -> Result<u16, RangeError> {
-        let value = match self.current.overflowing_add(amount) {
-            (_, true) => self.max,
-            (value, false) => value,
+    pub fn increase_by(&mut self, amount: u16) -> Result<u16, RangeError> {
+        let value = match self.value.checked_add(amount) {
+            Some(value) => value,
+            None => self.max,
         };
-        if value > self.max {
-            self.current = self.max;
-        } else {
-            self.current = value;
-        }
-
-        Ok(self.current)
+        self.value = value.min(self.max);
+        Ok(self.value)
     }
     
-    pub fn decrease_by(mut self, amount: u16) -> Result<u16, RangeError> {
-        let value = match self.current.overflowing_sub(amount) {
-            (_, true) => self.min,
-            (value, false) => value,
+    pub fn decrease_by(&mut self, amount: u16) -> Result<u16, RangeError> {
+        let value = match self.value.checked_sub(amount) {
+            None => self.min,
+            Some(value) => value,
         };
-        if value < self.min {
-            self.current = self.min;
-        } else {
-            self.current = value;
-        }
-
-        Ok(self.current)
+        self.value = value.min(self.max);
+        Ok(self.value)
     }
     
-    pub fn randomize(mut self) -> Result<u16, RangeError> {
+    pub fn randomize(&mut self) -> Result<u16, RangeError> {
         // How is it possible to test an unpredictable result?
         // Am I overlooking a possible error?
-        self.current = rand::thread_rng().gen_range(self.min, self.max + 1);
-        Ok(self.current)
+        self.value = rand::thread_rng().gen_range(self.min, self.max + 1);
+        Ok(self.value)
     }
    
-    pub fn maximize(mut self) -> Result<u16, RangeError> {
+    pub fn maximize(&mut self) -> Result<u16, RangeError> {
         // Am I overlooking a possible error?
-        self.current = self.max;
-        Ok(self.current)
+        self.value = self.max;
+        Ok(self.value)
     }
 
-    pub fn minimize(mut self) -> Result<u16, RangeError> {
+    pub fn minimize(&mut self) -> Result<u16, RangeError> {
         // Am I overlooking a possible error?
-        self.current = self.min;
-        Ok(self.current)
+        self.value = self.min;
+        Ok(self.value)
     }
 }
 
@@ -129,13 +86,5 @@ impl fmt::Display for RangeError {
 impl std::error::Error for RangeError {
     fn description(&self) -> &str {
         &self.message
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,106 +1,106 @@
-use range_pointer::Range;
+use range_pointer::BoundedValue;
 use std::u16;
 
 #[test]
 pub fn test_new() {
     assert_eq!(
-        Range{min: 1, max: 6, current: 1}, 
-        Range::new(1, 6, 1)
+        BoundedValue{min: 1, max: 6, value: 1}, 
+        BoundedValue::new(1, 6, 1)
     );
 }
 
 #[test]
 pub fn test_increase() {
-    let r = Range{min: 1, max: 6, current: 1};
+    let mut r = BoundedValue::new(1, 6, 1);
     let a = r.increase().unwrap();
     assert_eq!(a, 2);
 }
 
 #[test]
 pub fn test_increase_limit() {
-    let r = Range{min: 1, max: 6, current: 6};
+    let mut r = BoundedValue::new(1, 6, 6);
     let a = r.increase().unwrap();
     assert_eq!(a, 6);
 }
 
 #[test]
 pub fn test_increase_overflow() {
-    let r = Range{min: 1, max: u16::MAX, current: u16::MAX};
+    let mut r = BoundedValue::new(1, u16::MAX, u16::MAX);
     let a = r.increase().unwrap();
     assert_eq!(a, u16::MAX);
 }
 
 #[test] 
 pub fn test_increase_by() {
-    let r = Range{min: 1, max: 6, current: 1};
+    let mut r = BoundedValue::new(1, 6, 1);
     let a = r.increase_by(3).unwrap();
     assert_eq!(a, 4);
 }
 
 #[test] 
 pub fn test_increase_by_limit() {
-    let r = Range{min: 1, max: 6, current: 1};
+    let mut r = BoundedValue::new(1, 6, 1);
     let a = r.increase_by(10).unwrap();
     assert_eq!(a, 6);
 }
 
 #[test] 
 pub fn test_increase_by_overflow() {
-    let r = Range{min: 1, max: u16::MAX, current: u16::MAX};
+    let mut r = BoundedValue::new(1, u16::MAX, u16::MAX);
     let a = r.increase_by(10).unwrap();
     assert_eq!(a, u16::MAX);
 }
 
 #[test]
 pub fn test_decrease() {
-    let r = Range{min: 1, max: 6, current: 3};
+    let mut r = BoundedValue::new(1, 6, 3);
     let a = r.decrease().unwrap();
     assert_eq!(a, 2);
 }
 
 pub fn test_decrease_limit() {
-    let r = Range{min: 1, max: 6, current: 1};
+    let mut r = BoundedValue::new(1, 6, 1);
     let a = r.decrease().unwrap();
     assert_eq!(a, 1);
 }
 
 pub fn test_decrease_overflow() {
-    let r = Range{min: u16::MIN, max: 6, current: u16::MIN};
+    let mut r = BoundedValue::new(u16::MIN, 6, u16::MIN);
     let a = r.decrease().unwrap();
     assert_eq!(a, u16::MIN);
 }
 
 #[test] 
 pub fn test_decrease_by() {
-    let r = Range{min: 1, max: 6, current:5};
+    let mut r = BoundedValue::new(1, 6, 5);
     let a = r.decrease_by(2).unwrap();
     assert_eq!(a, 3);
 }
 
 #[test] 
 pub fn test_decrease_by_limit() {
-    let r = Range{min: 1, max: 6, current: 5};
+    let mut r = BoundedValue::new(1, 6, 5);
     let a = r.decrease_by(10).unwrap();
     assert_eq!(a, 1);
 }
 
 #[test] 
 pub fn test_decrease_by_overflow() {
-    let r = Range{min: u16::MIN, max: u16::MAX, current: u16::MIN};
+    let mut r = BoundedValue::new(u16::MIN, u16::MAX, u16::MIN);
     let a = r.decrease_by(10).unwrap();
     assert_eq!(a, u16::MIN);
 }
 
 #[test] 
 pub fn test_maximize() {
-    let r = Range{min: 1, max: 6, current: 1};
+    let mut r = BoundedValue::new(1, 6, 1);
     let a = r.maximize().unwrap();
     assert_eq!(a, 6);
 }
 
 #[test] 
 pub fn test_minimize() {
-    let r = Range{min: 1, max: 6, current: 5};
+    let mut r = BoundedValue::new(1, 6, 5);
     let a = r.minimize().unwrap();
     assert_eq!(a, 1);
 }
